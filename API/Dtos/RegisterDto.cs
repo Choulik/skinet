c195d684ddeada179b1d196e7e285ca7b4d3795a
Lukas Choulik <choulikl@seznam.cz>
2021-08-02 22:07:42 +0200

    end of section 16

--- API/Dtos/RegisterDto.cs
+++ API/Dtos/RegisterDto.cs
@@ -1,9 +1,17 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace API.Dtos
 {
     public class RegisterDto
     {
+        [Required]
         public string DisplayName { get; set; }
+        [Required]
+        [EmailAddress]
         public string Email { get; set; }
+        [Required]
+        [RegularExpression("(?=^.{6,10}$)(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&amp;*()_+}{&quot;:;'?/&gt;.&lt;,])(?!.*\\s).*$", 
+            ErrorMessage = "Password must have 1 Upper, 1 Lower, 1 number, 1 non numberic and at lease 6 chars.")]
         public string Password { get; set; }
     }
 }
